9dcab7e857d43c0bb29da59307aee9ff40db03df
slickcodes <hartpaulisimo@gmail.com>
2022-07-31 22:30:19 +0100

    add Detail nav component

---
+++ src/components/ContactDetailsNav.svelte
@@ -0,0 +1,29 @@
+<nav class="details flex">
+	<ul class="details__container flex justify-between">
+		<li>+234 805 498 5823</li>
+		<li>Location: Somewere around Nigeria</li>
+		<li>Mon - Fri: 8:00 - 5:00</li>
+	</ul>
+</nav>
+
+<style lang="scss">
+	/* your styles go here */
+	@import './../styles/_config.scss';
+
+	.details {
+		justify-content: center;
+		background: $background-white;
+
+		&__container {
+			width: 100%;
+			padding: 0.6em 1em;
+			max-width: $max-width;
+		}
+	}
+
+	@media (max-width: 700px) {
+		.details {
+			display: none;
+		}
+	}
+</style>
